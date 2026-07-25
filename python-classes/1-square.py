191df7ce58416ecd91473155e40939b28f37b5be
iam-wafula <shelwafula4@gmail.com>
2026-07-26 01:18:12 +0300

    Add module and class documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Defines a Square class."""
+
+
 class Square:
+    """Defines a square."""
+
     def __init__(self, size):
+        """Initialize a square with a size."""
         self.__size = size
