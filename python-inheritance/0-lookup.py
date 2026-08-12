df7eec4f0e932b2d6777e0f78d229a5fdd10d586
iam-wafula <shelwafula4@gmail.com>
2026-08-12 13:07:03 +0300

    Add Python inheritance project

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def lookup(obj):
+    """Return a list of an object's attributes and methods."""
     return dir(obj)
--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 class MyList(list):
+    """A list with a method to print its elements sorted."""
+
     def print_sorted(self):
+        """Print the list sorted in ascending order."""
         print(sorted(self))
